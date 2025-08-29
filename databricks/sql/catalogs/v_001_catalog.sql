aef1e19496b306ed38d18e037128472efbf54029
Gaurav Keshri <gaurav.keshri@hotmail.com>
2025-08-29 14:44:31 +1000

    Update tables

--- databricks/sql/catalogs/v_001_catalog.sql
+++ databricks/sql/catalogs/v_001_catalog.sql
@@ -3,13 +3,20 @@ CREATE CATALOG IF NOT EXISTS retail_analytics
   MANAGED LOCATION 'abfss://de-portfolio@newstoragegaurav.dfs.core.windows.net/uc-managed/';
 
 -- Create schema
-CREATE SCHEMA IF NOT EXISTS retail_analytics.ecommerce;
+CREATE SCHEMA raw
+  MANAGED LOCATION 'abfss://de-portfolio@newstoragegaurav.dfs.core.windows.net/retail_analytics/_managed/raw/';
+CREATE SCHEMA bronze
+  MANAGED LOCATION 'abfss://de-portfolio@newstoragegaurav.dfs.core.windows.net/retail_analytics/_managed/bronze/';
+CREATE SCHEMA silver
+  MANAGED LOCATION 'abfss://de-portfolio@newstoragegaurav.dfs.core.windows.net/retail_analytics/_managed/silver/';
+CREATE SCHEMA gold
+  MANAGED LOCATION 'abfss://de-portfolio@newstoragegaurav.dfs.core.windows.net/retail_analytics/_managed/gold/';
 
 -- Point to the catalog for following statements
 USE CATALOG retail_analytics;
-USE SCHEMA ecommerce;
 
--- 4) External locations
+
+-- Create External locations
 CREATE EXTERNAL LOCATION IF NOT EXISTS retail_bronze
   URL 'abfss://de-portfolio@newstoragegaurav.dfs.core.windows.net/bronze/'
   WITH (STORAGE CREDENTIAL `de-portfolio-ext-sc`)
